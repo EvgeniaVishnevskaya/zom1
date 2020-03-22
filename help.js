832743481a6565de86781877824de7f5c0aca170
EvgeniaVishnevskaya <55274706+EvgeniaVishnevskaya@users.noreply.github.com>
2020-03-22 22:26:51 +0300

    05 mobile

--- help.js
+++ help.js
@@ -1,3 +1,21 @@
 window.onmousemove = function (event) {
 
 }
+
+
+document.onmousemove = function (event) {
+  let x = event.x - 100;
+  let y = event.y - 100;
+  console.log(x+' '+y);
+  document.querySelector('#cl').style.transform = 'rotate(' + 100*arcctg(x, y) + 'deg)';
+
+  function arcctg(x, y) {
+
+    if (x > 0 && y > 0) return Math.PI / 2 - Math.atan(x / y);
+    if (x < 0 && y > 0) return Math.PI / 2 - Math.atan(x / y);
+    if (x < 0 && y < 0) return Math.PI +  Math.atan(x / y);
+    if (x > 0 && y < 0) return 3 * Math.PI / 2 + Math.abs(Math.atan(x / y));
+
+
+  }
+}
